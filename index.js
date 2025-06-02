2f8dba249fc8e60072ea4dbc84a4b765e35b7795
julkarzunayed <zunayedjulkar@gmail.com>
2025-06-02 14:44:59 +0600

    added application get methode by user id

--- index.js
+++ index.js
@@ -50,11 +50,22 @@ async function run() {
 
         app.get('/applications', async (req, res) => {
             const email = req.query.email;
-            console.log('Email:', email);
             const query = {
                 applicant: email
             };
             const result = await applicationCollection.find(query).toArray();
+
+            //bad way to aggregate data
+            for(const application of result ){
+                const jobId = application.jobId;
+                const jobQuery = {_id: new ObjectId(jobId)};
+                const job = await jobsCollection.findOne(jobQuery)
+                application.company = job.company;
+                application.title = job.title;
+                application.location = job.location;
+                application.company_logo = job.company_logo;
+
+            }
             res.send(result);
 
         })
